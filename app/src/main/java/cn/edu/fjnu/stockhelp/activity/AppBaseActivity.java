632fe19326fb9f9667df227d2abfe9f2e5633e2f
fly.gao <18396189097@163.com>
2017-09-08 20:13:01 +0800

    First upload files.
    Signed-off-by: fly.gao <18396189097@163.com>

---
+++ app/src/main/java/cn/edu/fjnu/stockhelp/activity/AppBaseActivity.java
@@ -0,0 +1,11 @@
+package cn.edu.fjnu.stockhelp.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017\9\2 0002.
+ * 应用基本Activity
+ */
+
+public class AppBaseActivity extends Activity {
+}
---
+++ app/src/main/java/cn/edu/fjnu/stockhelp/activity/BrowserActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.fjnu.stockhelp.activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+
+import cn.edu.fjnu.stockhelp.fragment.BrowserFragment;
+
+/**
+ * Created by Administrator on 2017\9\3 0003.
+ * 网页浏览页面
+ */
+
+public class BrowserActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment() {
+        return new BrowserFragment();
+    }
+}
